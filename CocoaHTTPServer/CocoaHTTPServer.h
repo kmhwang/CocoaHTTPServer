b1e2a91081d3fdaf12f4c239bde76e40a60ec5d7
Ken M. Hwang <Ken.M.Hwang@Gmail.com>
2017-11-07 10:33:53 +0800

    Carthage managed project

---
+++ CocoaHTTPServer/CocoaHTTPServer.h
@@ -0,0 +1,19 @@
+//
+//  CocoaHTTPServer.h
+//  CocoaHTTPServer
+//
+//  Created by Ken M. Hwang on 07/11/2017.
+//  Copyright © 2017 Ken M. Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaHTTPServer.
+FOUNDATION_EXPORT double CocoaHTTPServerVersionNumber;
+
+//! Project version string for CocoaHTTPServer.
+FOUNDATION_EXPORT const unsigned char CocoaHTTPServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaHTTPServer/PublicHeader.h>
+
+
